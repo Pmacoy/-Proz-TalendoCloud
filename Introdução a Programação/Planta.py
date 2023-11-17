b32914fc97f9335abd2c7df082ed3f98edead119
Pmacoy <pauloa.julio.com>
2023-11-17 20:34:56 +0000

    Inserção código planta

--- Introdução a Programação/Planta.py
+++ Introdução a Programação/Planta.py
@@ -6,15 +6,68 @@ while(contador < 6):
     contador += 1
 print('Termina programa rega while \n')
 
-print('Inicia programa tomate while')
+print('Inicia programa rega for')
+for i in range(6):
+    print('Regar planta nº: ', i)
+    i = i + 1
+print('Termina programa rega for \n')
+
+print('Inicia programa rega par while')
 contador = 0
 while(contador < 6):
     planta = contador
     print('Regar planta nº: ', planta)
     contador += 2
-print('Termina programa tomate while \n')
+print('Termina programa rega par while \n')
 
-print('Inicia programa tomate for')
+print('Inicia programa rega par for')
 for i in range(0,5,2):
     print('Regar planta nº: ', i)
     i = i + 2
+print('Termina programa rega par for \n')
+
+print('Inicia programa rega inversa while')
+contador = 5
+while(contador >= 0):
+    planta = contador
+    print('Regar planta nº: ', planta)
+    contador -= 1
+print('Termina programa rega inversa while \n')
+
+print('Inicia programa rega inversa for')
+for i in range(5, -1 , -1):
+    print('Regar planta nº: ', i)
+    i = i + 1
+print('Termina programa rega inversa for \n')
+
+print('Inicia programa rega confusa while')
+contador = 0
+while(contador < 6 ):
+    planta = contador
+    contador += 1
+    if planta in (2, 3):
+        continue
+    print('Regar planta nº: ', planta)
+print('Termina programa rega confusa while \n')
+
+print('Inicia programa rega confusa for')
+for planta in range(-1, 5):
+    if (planta == 1 or planta == 2):
+        continue
+    planta = planta + 1
+    print('Regar planta nº: ', planta)
+print('Termina programa rega confusa for \n')
+
+print('Inicia programa rega batata while')
+contador = 3
+while(contador < 6 ):
+    planta = contador
+    contador += 1
+    print('Regar planta nº: ', planta)
+print('Termina programa rega batata while \n')
+
+print('Inicia programa rega batata for')
+for planta in range(2, 5):
+    planta = planta + 1
+    print('Regar planta nº: ', planta)
+print('Termina programa rega batata for \n')

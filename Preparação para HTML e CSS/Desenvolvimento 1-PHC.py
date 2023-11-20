0a77c266340fcc2298a4a7451473a0cb2c1060b0
Pmacoy <pauloa.julio.com>
2023-11-20 15:12:27 +0000

    Atualização PHC

---
+++ Preparação para HTML e CSS/Desenvolvimento 1-PHC.py
@@ -0,0 +1,3 @@
+bk = ["Chicken Crispy", "Whooper", "Brutal Bacon", "Double Angus", "Smoky Steakhouse"]
+amigos = ["Pablo : 26/09", "Flora : 22/09", "Andryel : 04/12", "Keila : 21/06", "Ivanilda : 06/07" ]
+print("Presente:", bk[0], "Para:", amigos[2])

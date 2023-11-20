1f0cc6b658f9d46c88c8c64826d09b77209fcecb
Pmacoy <pauloa.julio.com>
2023-11-20 16:27:46 +0000

    Atualização Desenvolvimento 4

--- Preparação para HTML e CSS/Desenvolvimento 4-PHC.py
+++ Preparação para HTML e CSS/Desenvolvimento 4-PHC.py
@@ -1,4 +1,9 @@
 lista_produtos = ['máscaras faciais', 'batons', 'esmaltes', 'perfumes', 'loções', 'xampus', 'sabonetes', 'delineadores'] 
 
+lista_produtos.pop()
+lista_produtos.append('lapis de olho')
+lista_produtos.append("blush")
+lista_produtos[1], lista_produtos[4] = "rímel", "creme hidratante"
+print(lista_produtos)
 for i in range(len(lista_produtos)):
     print("Temos",lista_produtos[i],"à venda!")

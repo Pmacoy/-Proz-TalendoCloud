404c2f0f1d77f521336e3c272c526aaf3ad80c79
Pmacoy <pauloa.julio.com>
2023-11-20 16:10:28 +0000

    Atualização HTML e CSS

---
+++ Preparação para HTML e CSS/Desenvolvimento 2-PHC.py
@@ -0,0 +1,6 @@
+lista_musicos = [ 'Djavan', 'Roberto Carlos', 'Elis Regina', 'Tom Jobim', 'Milton Nascimento', 'Chico Buarque', 'Nara Leão', 'Pitty', 'Simonal', 'Moacir Santos', 'Caetano Veloso', 'Elza Soares', 'Paulinho da Viola', 'Yamandú Costa', 'Gal Costa'] 
+qtde_lista = len(lista_musicos)
+print("Quantidade de elementos que a lista possui:", qtde_lista)
+print("O dado salvo no índice 2: ", lista_musicos[2])
+print("O dado salvo no índice 9: ", lista_musicos[9])
+print("O dado salvo no índice 14: ", lista_musicos[14])

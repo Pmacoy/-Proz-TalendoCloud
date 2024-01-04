cef5f2422b867f066623087ded03b407c2435786
pmacoy <pauloa.julio@gmail.com>
2024-01-04 14:29:26 +0000

    update desenvolvimento

---
+++ Javascript I/primeiro-js/script.js
@@ -0,0 +1,7 @@
+let mostrar = ["Conexão feita com sucesso!", "Conexão feita com sucesso!", "Conexão feita com sucesso!"];
+for (let i = 0; i < 4; i++) {
+    console.log(mostrar[i]);
+};
+let num3 = 155;
+let num4 = 27;
+console.log(num3 + num4);

8ed8b151cf8ab991187bcd71e9416cfcaf54cdde
pmacoy <pauloa.julio@gmail.com>
2024-01-29 16:57:36 +0000

    update

---
+++ Javascript II/Prevent-Alert/script.js
@@ -0,0 +1,10 @@
+let linkProz = document.getElementById("link-proz")
+let btnSubmit = document.querySelector("button[type=submit]")
+
+linkProz.addEventListener("click", (e)=> {
+    
+})
+
+btnSubmit.addEventListener("click", (e)=> {
+    e.preventDefault()
+})
--- Javascript II/eventos-botao/script.js
+++ Javascript II/eventos-botao/script.js
@@ -3,15 +3,32 @@ let codeText = document.querySelector("#code-text");
 let quadrado = document.querySelector("#quadrado");
 let distanciaDaEsquerda = 0
 
-// document.addEventListener("keyup",(e)=> {
-//     keyText.innerText = e.key;
-// 	codeText.innerText = e.code;
-//     console.log(e);
-//     console.log(e.code);
-// })
+document.addEventListener("keyup",(e)=> {
+    keyText.innerText = e.key;
+  	codeText.innerText = e.code;
+    console.log(e);
+    console.log(e.code);
+})
 
-Document.addEventListener("keydown", (e) => {
+document.addEventListener("keydown", (e) => {
     if(e.code == "ArrowRight"){
 	    console.log("Apertou a seta à direita");
     }
 })
+
+document.addEventListener("keydown", (e) => {
+    if(e.code == "ArrowRight"){
+        distanciaDaEsquerda = distanciaDaEsquerda + 10;
+	    console.log(distanciaDaEsquerda);
+    }
+})
+
+document.addEventListener("keydown", (e) => {
+    if(e.code == "ArrowRight"){
+      distanciaDaEsquerda = distanciaDaEsquerda + 10
+      console.log(distanciaDaEsquerda);
+  
+      quadrado.style.left = distanciaDaEsquerda + "px"
+    }
+  })
+

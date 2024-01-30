116cb545c1094da4c451493e778ba680acdfb895
Pmacoy <paulo.julio@gmail.com>
2024-01-30 19:02:59 +0000

    update code

--- Javascript II/Prevent-Alert/script.js
+++ Javascript II/Prevent-Alert/script.js
@@ -2,9 +2,11 @@ let linkProz = document.getElementById("link-proz")
 let btnSubmit = document.querySelector("button[type=submit]")
 
 linkProz.addEventListener("click", (e)=> {
-    
+    e.preventDefault()
+    alert("Não foi possível acessar o link");
 })
 
 btnSubmit.addEventListener("click", (e)=> {
     e.preventDefault()
+    alert("Não foi possível enviar os dados do formulário")
 })
